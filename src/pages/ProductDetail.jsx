dbf413a86cfed9f5a43835b690ecc1367d7b18bb
jffelippe <jffelippe@hotmail.com>
2022-12-14 13:53:36 -0500

    Product, PagesProduct, ProductDetail y ServerProduct listos

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteProductById, findProductById, saveProduct } from "../server/ServerProduct.jsx";
+
+
+function ProductDetail() {
+
+    const [disabled, setDisable] = useState(true);
+    const [product, setProduct] = useState({})
+
+    const { id } = useParams();
+
+    const navigate = useNavigate();
+    function retornarPage() {
+        navigate("/product")
+    }
+
+    useEffect(() => {
+        findProductById(id)
+            .then(data => {
+                setProduct(data);
+            })
+    }, [id])
+
+    async function eliminarProduct(id) {
+        let respuesta = window.confirm("Seguro de Eliminar?");
+        if (respuesta) {
+            const response = await deleteProductById(id);
+            alert(response);
+            retornarPage();
+        }
+    };
+
+    function handleChange({ target }) {
+        setProduct({
+            ...product,
+            [target.name]: target.value
+        });
+    };
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const response = await saveProduct(product);
+        alert(response);
+        retornarPage()
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Form.Label>Id</Form.Label>
+                <Col><Form.Control defaultValue={product.id} disabled /></Col>
+                <Col md="auto"></Col>
+                <Col xs lg="2">
+                    <Button variant="danger" onClick={() => eliminarProduct(product.id)} >Eliminar</Button>
+                </Col>
+            </Row>
+
+            <Form className="mt-5" onSubmit={handleSubmit}>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="servicio">
+                        <Form.Label>Servicio</Form.Label>
+                        <Form.Control defaultValue={product.servicio} disabled={disabled}
+                                      name="servicio"
+                                      type="text"
+                                      required
+                                      onChange={handleChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="costo">
+                        <Form.Label>Costo</Form.Label>
+                        <Form.Control defaultValue={product.costo} disabled={disabled}
+                                      name="costo"
+                                      type="number"
+                                      required
+                                      onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="duracion">
+                        <Form.Label>Duración</Form.Label>
+                        <Form.Control defaultValue={product.duracion} disabled={disabled}
+                                      name="duracion"
+                                      type="number"
+                                      required
+                                      onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="idCliente">
+                        <Form.Label>Id Cliente</Form.Label>
+                        <Form.Control defaultValue={product.idCliente} disabled={disabled}
+                                      name="idCliente"
+                                      type="text"
+                                      required
+                                      onChange={handleChange}
+                        />
+                    </Form.Group>
+                    
+                </Row>
+                <Row>
+                    <Col md={{ span: 3, offset: 3 }}><Button variant="success" type="submit">Guardar</Button></Col>
+                    <Col md={{ span: 3, offset: 3 }}><Button variant="warning" onClick={() => setDisable(!disabled)}>Editar</Button></Col>
+                </Row>
+
+
+            </Form>
+            <Button variant="primary" type="button" onClick={retornarPage}>
+                Volver
+            </Button>
+        </Container>
+
+    )
+
+}
+export { ProductDetail }
--- src/pages/ProductForm.jsx
+++ src/pages/ProductForm.jsx
@@ -1,5 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { Button } from "react-bootstrap/lib/inputgroup";
+import { Form } from "react-bootstrap/lib/navbar";
+import { Container } from "react-bootstrap/lib/tab";
+import { useNavigate } from "react-router-dom";
 
 function Productform(){
 
+    const navigate = useNavigate();
+    function returntoProduct(){
+        navigate("/product")
+    }
+
+    const [product, setProduct] = useState({
+        
+        service:"",
+        cost:"",
+        durationService:"",
+        idClient:""
+    });
+
+    function handleChange({target}){
+        setProduct({
+            ...product,
+            [target.name]:target.value
+        });
+    };
+
+    async function handleSubmit(e){
+        e.preventdefault();
+        const response = await saveProduct(product);
+        alert(response);
+        returntoProduct;
+    }
+
+    return (
+        <Container>
+            <h2>Registrar Producto</h2>
+            <Form onSubmit={handleSubmit}>
+                <Row>
+                    <Col xs="auto" className="my-1">
+                        <Form.label1>Servicio</Form.label1>
+                        <Form.Control
+                        required
+                        type = "text"
+                        name="service"
+                        onChange={handleChange}
+                        />
+                    </Col>
+                    <Col xs="auto" className="my-1">
+                    <Form.label1>Costo</Form.label1>
+                        <Form.Control
+                        required
+                        type = "number"
+                        name="cost"
+                        onChange={handleChange}
+                        />
+                    </Col>
+                    <Col xs="auto" className="my-1">
+                    <Form.label1>Duración servicio</Form.label1>
+                        <Form.Control
+                        required
+                        type = "number"
+                        name="durationService"
+                        onChange={handleChange}
+                        />
+                    </Col>
+                    <Row>
+                    </Row>
+                    <Col xs="auto" className="my-1">
+                    <Form.label1>ID Cliente</Form.label1>
+                        <Form.Control
+                        required
+                        type = "text"
+                        name="idClient"
+                        onChange={handleChange}
+                        />
+                    </Col>
+                </Row>
+                <Row></Row>
+                <Row>
+                    <Col md={{span:3, offset:3}}> <Button variant="success" type="submit">Guardar</Button></Col>
+                </Row>
+            </Form>
+            <Row xs={2} md={4} lg={6} className="my-3">
+                <Col><Button onClick={returntoProduct}>Volver</Button> </Col>
+            </Row>
+        </Container>
+    )
 }export {Productform}
